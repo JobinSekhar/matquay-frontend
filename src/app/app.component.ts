b1b0447dae969cfaa76b974f4d123baaeb2fe74c
JobinSekhar <jobinsekhar@xminds.in>
2021-01-12 17:18:52 +0530

    employee add edit fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {EmployeeService} from "./service/employeeService";
+import {first} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  employeeForm: FormGroup;
+  employees:any
+
+  constructor(
+      private formBuilder: FormBuilder,
+      private empService: EmployeeService) {
+  }
+
+  ngOnInit() {
+    this.employeeForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address: ['', Validators.required],
+      state: ['', Validators.required],
+      country: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      department: ['', Validators.required],
+    });
+    this.getEmployeesList();
+  }
+
+  onSubmit() {
+    this.empService.addEmployee(this.employeeForm.value).pipe(first())
+        .pipe().subscribe(
+            data => {
+             console.log("success")
+            });
+  }
+
+  getEmployeesList() {
+      this.empService.getEmployeeList().pipe(first())
+          .pipe().subscribe(
+          data => {
+              this.employees = data;
+          });
+  }
+
+  deleteEmployee(employeeId) {
+      this.empService.deleteEmployee(employeeId).pipe(first())
+          .pipe().subscribe(
+          data => {
+              console.log("Success")
+              this.getEmployeesList();
+          });
+  }
 }
